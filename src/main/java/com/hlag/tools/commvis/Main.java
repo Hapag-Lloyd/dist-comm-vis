0a2a28bce4d287d3622d33d051cf4186f84b03b2
kayma <kayma@hlag.com>
2022-04-21 14:36:29 +0200

    feat: add Slf4j

--- src/main/java/com/hlag/tools/commvis/Main.java
+++ src/main/java/com/hlag/tools/commvis/Main.java
@@ -1,22 +1,25 @@
 package com.hlag.tools.commvis;
 
+import lombok.extern.slf4j.Slf4j;
 import picocli.CommandLine;
+
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name="DistCommVis", mixinStandardHelpOptions = true, description = "Analyzes the classpath and extracts endpoints and event sender/receiver.")
+@Slf4j
+@CommandLine.Command(name = "DistCommVis", mixinStandardHelpOptions = true, description = "Analyzes the classpath and extracts endpoints and event sender/receiver.")
 public class Main implements Callable<Integer> {
+    @CommandLine.Parameters(index = "0", description = "The root package to analyze (including all sub packages)")
+    private String packageName;
+
     public static void main(String... args) {
         int exitCode = new CommandLine(new Main()).execute(args);
 
         System.exit(exitCode);
     }
 
-    @CommandLine.Parameters(index = "0", description = "The root package to analyze (including all sub packages)")
-    private String packageName;
-
     @Override
     public Integer call() throws Exception {
-        System.out.print("The main method");
+        log.info("The main method");
 
         return 0;
     }
