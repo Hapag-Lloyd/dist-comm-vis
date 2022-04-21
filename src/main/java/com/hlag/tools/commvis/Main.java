753f825443e9d9b3bdafacccb8720257241e1f5e
kayma <kayma@hlag.com>
2022-04-21 12:30:55 +0200

    feat: add CLI parser

--- src/main/java/com/hlag/tools/commvis/Main.java
+++ src/main/java/com/hlag/tools/commvis/Main.java
@@ -1,7 +1,23 @@
 package com.hlag.tools.commvis;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("The main method");
+import picocli.CommandLine;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name="DistCommVis", mixinStandardHelpOptions = true, description = "Analyzes the classpath and extracts endpoints and event sender/receiver.")
+public class Main implements Callable<Integer> {
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+
+        System.exit(exitCode);
+    }
+
+    @CommandLine.Parameters(index = "0", description = "The root package to analyze (including all sub packages)")
+    private String packageName;
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.print("The main method");
+
+        return 0;
     }
 }
