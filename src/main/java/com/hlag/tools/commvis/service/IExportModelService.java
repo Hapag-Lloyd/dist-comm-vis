e97ca48d4f54d59f96918215e07cb9469d47eea5
kayma <kayma@hlag.com>
2022-04-28 10:42:19 +0200

    build: fix the pom and add sources and javadocs

--- src/main/java/com/hlag/tools/commvis/service/IExportModelService.java
+++ src/main/java/com/hlag/tools/commvis/service/IExportModelService.java
@@ -8,6 +8,8 @@ import com.hlag.tools.commvis.domain.model.CommunicationModel;
 public interface IExportModelService {
     /**
      * Converts the internal model and stores it as file.
+     * @param model the model to export
+     * @param filename the output file name without an extension
      */
     void export(CommunicationModel model, String filename);
 }
