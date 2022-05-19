da887b4d1c6e16fe854fb9a2eb1142fd50f2c508
kayma <kayma@hlag.com>
2022-05-19 11:08:36 +0200

    COMBINE-FILES

--- src/test/java/com/hlag/tools/commvis/adapter/in/CommunicationModelFromJsonFileAdapterTest.java
+++ src/test/java/com/hlag/tools/commvis/adapter/in/CommunicationModelFromJsonFileAdapterTest.java
@@ -1,15 +1,18 @@
 package com.hlag.tools.commvis.adapter.in;
 
 import com.hlag.tools.commvis.analyzer.model.CommunicationModel;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
 
+@Slf4j
 class CommunicationModelFromJsonFileAdapterTest {
     private CommunicationModelFromJsonFileAdapter adapter;
 
@@ -20,8 +23,8 @@ class CommunicationModelFromJsonFileAdapterTest {
 
     @Test
     void shouldFindAllModelFilesInSubDirectories_when() {
-        //Path p = Paths.get("src/test/resources/json-file-adapter/");
-        Path p = Paths.get("/home/runner/work/dist-comm-vis/dist-comm-vis/src/test/resources/json-file-adapter");
+        log.info("SEPARATOR: {}", File.separator);
+        Path p = Paths.get("src/test/resources/json-file-adapter/");
         String givenBaseDirectory = p.toFile().getAbsolutePath();
 
         Collection<Path> actualModelFiles = adapter.getModelFiles(givenBaseDirectory);
