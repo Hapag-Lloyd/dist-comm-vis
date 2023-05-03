82457e29b8abdbbc2bffc1b406ada465c3bd72fd
Matthias Kay <matthias.kay@hlag.com>
2023-05-03 15:32:45 +0200

    PYTHON-REWRITE

--- tests/dist_comm_vis/domain/service/test_java_file_analyzer_service.py
+++ tests/dist_comm_vis/domain/service/test_java_file_analyzer_service.py
@@ -1,4 +1,4 @@
-import ntpath
+import os
 
 from dist_comm_vis.adapter.service.LocalFileReaderService import LocalFileReaderService
 from dist_comm_vis.definitions import ROOT_DIR
@@ -8,10 +8,8 @@ from dist_comm_vis.domain.service.FileAnalyzerService import JavaFileAnalyzerSer
 
 def test_detects_model_string_in_double_slash_comment():
     # given
-    given_local_path = ntpath.abspath(ntpath.join(ROOT_DIR, "tests/data/source/java"))
-    given_file = File(ntpath.join(given_local_path, "Class1.java"))
-
-    assert "a" == given_file.full_qualified_name
+    given_local_path = os.path.abspath(os.path.join(ROOT_DIR, "tests/data/source/java"))
+    given_file = File(os.path.join(given_local_path, "Class1.java"))
 
     # when
     actual_model = JavaFileAnalyzerService(LocalFileReaderService()).detect_model_relations(given_file)
