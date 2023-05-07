d30db5fe9ac933b784abfc97cefa9f6e7b23a9e3
Matthias Kay <matthias.kay@hlag.com>
2023-05-07 09:47:13 +0200

    PYTHON-REWRITE

--- tests/dist_comm_vis/adapter/endpoint/test_cli.py
+++ tests/dist_comm_vis/adapter/endpoint/test_cli.py
@@ -26,7 +26,7 @@ def test_log_config_file_parameter_accepted():
 
 def test_all_parameters_are_present_in_container_dictionary():
     # given
-    given_all_args = ["my-path", "--log-config-file", "my-log-config-file"]
+    given_all_args = ["my-path", "--log-config-file", "my-log-config-file", "--output-path", "my-output-path"]
 
     # when
     actual_args = create_configuration_values_from_command_line_arguments(parse_command_line_arguments(given_all_args))
@@ -34,5 +34,6 @@ def test_all_parameters_are_present_in_container_dictionary():
     # then
     assert actual_args == {
         "path": "my-path",
+        "output_path": "my-output-path",
         "log_config_file": "my-log-config-file"
     }
